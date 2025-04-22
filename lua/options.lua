b38ec8f99b5c7579a6eca663da471c688ac791fe
Vlad Silin <vlad.silin.y@gmail.com>
2025-04-22 14:43:57 -0400

    Change tab char settings

--- lua/options.lua
+++ lua/options.lua
@@ -16,7 +16,7 @@ vim.opt.incsearch = true -- do incremental searching
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 
 -- Set advanced autocomplete
 vim.opt.wildmenu = true
@@ -101,7 +101,8 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '- ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
