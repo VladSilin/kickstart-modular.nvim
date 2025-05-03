779908ee63a51df2adfff55860347662cfd20e15
Vlad Silin <vlad.silin.y@gmail.com>
2025-05-03 18:54:45 -0400

    Add copilot stuff

---
+++ lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    init = function()
+      vim.keymap.set('n', '<leader>cc', function()
+        require('CopilotChat').toggle()
+      end, { desc = 'Toggle Copilot Chat' })
+    end,
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
