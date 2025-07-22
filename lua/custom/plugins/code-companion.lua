ead6843ffe008f6f354dd9c46dd3b6d89f4d16f5
Vlad Silin <vlad.silin.y@gmail.com>
2025-07-22 10:38:49 -0400

    Add CodeCompanion AI plugin to replace CopilotChat

---
+++ lua/custom/plugins/code-companion.lua
@@ -0,0 +1,36 @@
+return {
+  'olimorris/codecompanion.nvim',
+  opts = {
+    strategies = {
+      chat = {
+        adapter = 'copilot',
+      },
+      inline = {
+        adapter = 'copilot',
+        -- accept_change = {
+        --   modes = { n = 'ccy' },
+        --   description = 'Accept the suggested change',
+        -- },
+        -- reject_change = {
+        --   modes = { n = 'ccn' },
+        --   description = 'Reject the suggested change',
+        -- },
+      },
+    },
+  },
+  init = function()
+    vim.keymap.set({ 'n', 'v' }, '<Leader>cc', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
+    vim.keymap.set('v', '<Leader>ccv', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
+
+    vim.keymap.set({ 'n', 'v' }, '<Leader>cci', '<cmd>CodeCompanion<cr>', { noremap = true, silent = true })
+
+    vim.keymap.set({ 'n', 'v' }, '<Leader>cca', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd [[cab cc CodeCompanion]]
+  end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
