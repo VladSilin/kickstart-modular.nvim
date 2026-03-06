1bd9307253c28adb8c70dba8f65aac650bd2b4fe
Vlad Silin <vlad.silin.y@gmail.com>
2026-03-05 21:04:11 -0500

    feat: add vite icons

--- lua/custom/plugins/nvim-web-devicons.lua
+++ lua/custom/plugins/nvim-web-devicons.lua
@@ -2,6 +2,38 @@ return {
   'nvim-tree/nvim-web-devicons',
   opts = {
     strict = true,
+    override_by_filename = {
+      ['vite.config.ts'] = {
+        icon = '󱐋',
+        color = '#FFAB00',
+        name = 'Vite',
+      },
+      ['vite.config.js'] = {
+        icon = '󱐋',
+        color = '#FFAB00',
+        name = 'Vite',
+      },
+      ['vite.config.mts'] = {
+        icon = '󱐋',
+        color = '#FFAB00',
+        name = 'Vite',
+      },
+      ['vitest.config.ts'] = {
+        icon = '󱐋',
+        color = '#6E9F18',
+        name = 'Vitest',
+      },
+      ['vitest.config.js'] = {
+        icon = '󱐋',
+        color = '#6E9F18',
+        name = 'Vitest',
+      },
+      ['vitest.config.mts'] = {
+        icon = '󱐋',
+        color = '#6E9F18',
+        name = 'Vitest',
+      },
+    },
     override_by_extension = {
       ['astro'] = {
         icon = '',
